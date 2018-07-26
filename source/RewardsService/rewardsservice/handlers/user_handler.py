c2834c411340dace5546b647ca11c6a75aaa96ac
spiskommg <spisko@maymediagroup.com>
2018-07-26 14:10:43 -0400

    build out of user rewards endpoint which accepts rewards data including email and order_total to be processed for rewards tier logic. The post request which accepts these values searches the database for a record based on the email address passed into this method. Depending on if a user is found the method either creates the first instance of this user in the database setting their reward tier and next tier based on the initial order_total passed in. I created a method for querying the tier position based on the user order total inorder to easily access the query and prevent duplication, and ensure that we always get the same result when asking the database for the users current position. When a user goes beyond the available reward levels i added a message to user reward level record which identifies that they have reached the reward ceiling. This value may change later as I integrate this further with the dashboard ui

---
+++ source/RewardsService/rewardsservice/handlers/user_handler.py
@@ -0,0 +1,93 @@
+import json
+import tornado.web
+
+from pymongo import MongoClient
+from tornado.gen import coroutine
+
+
+class UserRewardsHandler(tornado.web.RequestHandler):
+
+    @coroutine
+    def get(self):
+        client = MongoClient("mongodb", 27017)
+        db = client["UserRewards"]
+        user_rewards = list(db.user_rewards.find({}, {"_id": 0}))
+        self.write(json.dumps(user_rewards))
+
+    def post(self):
+        """
+            Recieve user order details
+            should be a json object containing email, and order_total parameters
+        """
+        email = self.get_argument('email')
+        order_total = round(float(self.get_argument('order_total')), 0)
+
+        """
+            Connect to MongoDB
+        """
+        client = MongoClient("mongodb", 27017)
+        """  """
+        db = client["UserRewards"]
+        db = client["Rewards"]
+
+        """ Find related user object """
+        user_rewards = list(db.user_rewards.find({"email": email}, {"_id": 0, "email": 1, "points": 1, "tier": 1, "rewardName": 1, "nextTier": 1, "nextRewardName": 1, "tierProgress": 1}).limit(1))[0]
+
+        if not user_rewards:
+                # self.write("NO USER, need create %s %.0f" % (email, order_total))
+
+                """ GET REWARD WHICH IS LESS THAN THE ORDER TOTAL sent with request """
+                currRewards, nextRewards = self.get_tier_position(order_total)
+
+                new_user = {
+                    "email": email, # Email Address: the customer's email address (ex. "customer01@gmail.com")
+                    "points": order_total, # Reward Points: the customer's rewards points (ex. 100)
+                    "tier": currRewards['tier'], # Rewards Tier: the rewards tier the customer has reached (ex. "A")
+                    "rewardName": currRewards['rewardName'], # Reward Tier Name: the name of the rewards tier (ex. "5% off purchase")
+                    "nextTier": nextRewards['tier'], # Next Rewards Tier: the next rewards tier the customer can reach (ex. "B")
+                    "nextRewardName": nextRewards['rewardName'], # Next Rewards Tier Name: the name of next rewards tier (ex. "10% off purchase")
+                    "tierProgress": self.calc_progress(float(currRewards['points']),float(nextRewards['points']),order_total) # Next Rewards Tier Progress: the percentage the customer is away from reaching the next rewards tier (ex. 0.5)
+                }
+                # self.write(json.dumps(new_user))
+                insert_res = db.user_rewards.insert_one(new_user)
+                self.write(json.dumps(insert_res))
+        else:
+                # self.write(json.dumps(user_rewards))
+
+                """ There is a user matching the post request, now need to add the new order total """
+                updated_user_points = float(user_rewards['points']) + order_total
+                """ GET REWARD WHICH IS LESS THAN THE ORDER TOTAL sent with request """
+                currRewards, nextRewards = self.get_tier_position(updated_user_points)
+
+                """
+                    UPDATE the user with the new values
+                    based on the query from get_tier_position
+                    which returns the new current and next tiers
+                """
+
+                updated_user = {
+                    "email": email, # Email Address: the customer's email address (ex. "customer01@gmail.com")
+                    "points": updated_user_points, # Reward Points: the customer's rewards points (ex. 100)
+                    "tier": currRewards['tier'], # Rewards Tier: the rewards tier the customer has reached (ex. "A")
+                    "rewardName": currRewards['rewardName'], # Reward Tier Name: the name of the rewards tier (ex. "5% off purchase")
+                    "nextTier": nextRewards['tier'] if nextRewards else 'Reached Reward Limit', # Next Rewards Tier: the next rewards tier the customer can reach (ex. "B")
+                    "nextRewardName": nextRewards['rewardName'] if nextRewards else 'Reached Reward Limit', # Next Rewards Tier Name: the name of next rewards tier (ex. "10% off purchase")
+                    "tierProgress": self.calc_progress(float(currRewards['points']),float(nextRewards['points']),updated_user_points) if nextRewards else 'Reached Reward Limit' # Next Rewards Tier Progress: the percentage the customer is away from reaching the next rewards tier (ex. 0.5)
+                }
+                self.write(json.dumps(updated_user))
+
+                newvalues = { "$set": updated_user }
+
+                update_res = db.user_rewards.update_one(user_rewards, newvalues)
+                print(update_res)
+
+
+    def calc_progress(self, curr_tier, next_tier, user_points):
+        return (user_points - curr_tier) / (next_tier - curr_tier)
+
+    def get_tier_position(self, order_total):
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        currRewards = list(db.rewards.find({'points':{'$lt':order_total}}, {'_id':0}).sort([('points', -1)]).limit(1))
+        nextRewards = list(db.rewards.find({'points':{'$gt':order_total}}, {'_id':0}).sort([('points', 1)]).limit(1))
+        return currRewards[0] if len(currRewards) > 0 else None, nextRewards[0] if len(nextRewards) > 0 else None
